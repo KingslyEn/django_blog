5900a7d4a21e2514a68adf631542af26a3d2efcb
KingslyEn <KingslyEn98@gmail.com>
2022-04-20 13:49:00 +0430

    Adding static data to home page. (pretend database values)

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'KingslyEn',
+        'title': 'First Post 9898',
+        'content': 'Hey there, this is the first static post.',
+        'date_posted': '24 October 2021'
+    },
+    {
+        'author': 'Rj009',
+        'title': 'Sec one fro RJ',
+        'content': 'Yo yo, and the is the sec one from rj.',
+        'date_posted': '1 July 2022'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("I'm learning django :) just this")
+    return render(request, 'blog/about.html', {'title': 'About page'})
