1dce416bfa9ffebbeef8572ce5c8d286c53357f5
KingslyEn <KingslyEn98@gmail.com>
2022-04-21 17:46:36 +0430

    Remove dummy data and instead using database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'KingslyEn',
-        'title': 'First Post 9898',
-        'content': 'Hey there, this is the first static post.',
-        'date_posted': '24 October 2021'
-    },
-    {
-        'author': 'SNB78',
-        'title': 'Second post from SNB',
-        'content': 'Yo yo, and this is the second post from SNB.',
-        'date_posted': '1 July 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
